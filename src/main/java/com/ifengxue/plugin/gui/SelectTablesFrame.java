234ae9cabee206ee45089d536d70bb2f8181013e
liukefeng <liukefeng2008@hotmail.com>
2019-03-30 13:47:09 +0800

    rewrite LogChute Avoid access denied exceptions (velocity.log) #4

--- src/main/java/com/ifengxue/plugin/gui/SelectTablesFrame.java
+++ src/main/java/com/ifengxue/plugin/gui/SelectTablesFrame.java
@@ -49,6 +49,9 @@ import javax.swing.JTable;
 import javax.swing.WindowConstants;
 import javax.swing.table.AbstractTableModel;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.RuntimeServices;
+import org.apache.velocity.runtime.log.LogChute;
 
 public class SelectTablesFrame {
 
@@ -198,6 +201,61 @@ public class SelectTablesFrame {
 
       // repository
       VelocityEngine velocityEngine = new VelocityEngine();
+      // rewrite LogChute Avoid access denied exceptions (velocity.log)
+      // link: https://github.com/liukefeng2008/idea-plugin-jpa-support/issues/4
+      velocityEngine.setProperty(RuntimeConstants.RUNTIME_LOG_LOGSYSTEM, new LogChute() {
+        @Override
+        public void init(RuntimeServices runtimeServices) {
+
+        }
+
+        @Override
+        public void log(int level, String message) {
+          logInternal(level, message, null);
+        }
+
+        @Override
+        public void log(int level, String message, Throwable ex) {
+          logInternal(level, message, ex);
+        }
+
+        private void logInternal(int level, String message, Throwable ex) {
+          switch (level) {
+            case LogChute.TRACE_ID:
+              log.trace(message);
+              return;
+            case LogChute.DEBUG_ID:
+              log.debug(message, ex);
+              return;
+            case LogChute.INFO_ID:
+              log.info(message, ex);
+              return;
+            case LogChute.WARN_ID:
+              log.warn(message, ex);
+              return;
+            case LogChute.ERROR_ID:
+              log.error(message, ex);
+              return;
+            default:
+              log.error("unknown log level " + level + ", raw log message is " + level, ex);
+          }
+        }
+
+        @Override
+        public boolean isLevelEnabled(int level) {
+          switch (level) {
+            case LogChute.TRACE_ID:
+              return log.isTraceEnabled();
+            case LogChute.DEBUG_ID:
+              return log.isDebugEnabled();
+            case LogChute.INFO_ID:
+            case LogChute.WARN_ID:
+            case LogChute.ERROR_ID:
+            default:
+              return true;
+          }
+        }
+      });
       String encoding = StandardCharsets.UTF_8.name();
       velocityEngine.addProperty("input.encoding", encoding);
       velocityEngine.addProperty("output.encoding", encoding);
