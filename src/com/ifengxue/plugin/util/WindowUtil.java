c2bce451d55f01148c46b10285ed5b2f7fc6fff0
Carter <liukefeng2008@hotmail.com>
2020-04-19 16:25:55 +0800

    Fix compatibility issues with some idea tips

--- src/com/ifengxue/plugin/util/WindowUtil.java
+++ src/com/ifengxue/plugin/util/WindowUtil.java
@@ -3,7 +3,6 @@ package com.ifengxue.plugin.util;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.WindowManager;
 import com.intellij.openapi.wm.ex.WindowManagerEx;
-import com.intellij.openapi.wm.impl.IdeFrameImpl;
 import java.awt.Window;
 
 public enum WindowUtil {
@@ -14,7 +13,7 @@ public enum WindowUtil {
     Window window = windowManager.suggestParentWindow(project);
     if (window == null) {
       Window focusedWindow = windowManager.getMostRecentFocusedWindow();
-      if (focusedWindow instanceof IdeFrameImpl) {
+      if (focusedWindow != null) {
         window = focusedWindow;
       }
     }
