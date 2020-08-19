52fa8a3e670d6a9eb3b36c9eb071bf5f6e56c482
Carter <liukefeng@souche.com>
2020-08-19 22:16:54 +0800

    增加ColumnFieldMappingEditor

---
+++ src/main/java/com/ifengxue/plugin/component/ColumnFieldMappingEditor.java
@@ -0,0 +1,11 @@
+package com.ifengxue.plugin.component;
+
+import lombok.Data;
+
+import javax.swing.*;
+
+@Data
+public class ColumnFieldMappingEditor {
+  private JTextField textClassname;
+  private JTextField textRepositoryName;
+}
