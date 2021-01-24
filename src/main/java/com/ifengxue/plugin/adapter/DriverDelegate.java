848e52eb1a44bcf12bbbf224e58d746abae118f3
Carter <liukefeng@huimin100.cn>
2021-01-24 11:56:21 +0800

    fix driver adapter acceptUrl incorrect bug

--- src/main/java/com/ifengxue/plugin/adapter/DriverDelegate.java
+++ src/main/java/com/ifengxue/plugin/adapter/DriverDelegate.java
@@ -1,13 +1,20 @@
 package com.ifengxue.plugin.adapter;
 
+import com.ifengxue.plugin.Holder;
 import java.sql.Driver;
+import java.sql.SQLException;
 import lombok.Getter;
 import lombok.experimental.Delegate;
 
 public class DriverDelegate implements Driver {
 
-  @Delegate
+  private interface ExcludeMethods {
+
+    boolean acceptsURL(String url) throws SQLException;
+  }
+
   @Getter
+  @Delegate(excludes = ExcludeMethods.class)
   private final Driver driver;
   @Getter
   private final DatabaseDrivers databaseDrivers;
@@ -16,4 +23,10 @@ public class DriverDelegate implements Driver {
     this.driver = driver;
     this.databaseDrivers = databaseDrivers;
   }
+
+  @Override
+  public boolean acceptsURL(String url) throws SQLException {
+    return Holder.getDatabaseDrivers() == databaseDrivers && driver.acceptsURL(url);
+  }
+
 }
