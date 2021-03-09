c22b841ca2784e3e682a46485f0924ed01869859
Carter <liukefeng2008@hotmail.com>
2021-03-09 21:37:00 +0800

    feat(file): UI中增加文件重复时的动作选项

--- src/main/java/com/ifengxue/plugin/component/SelectTables.java
+++ src/main/java/com/ifengxue/plugin/component/SelectTables.java
@@ -2,6 +2,7 @@ package com.ifengxue.plugin.component;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import lombok.Data;
 
 @Data
@@ -15,4 +16,7 @@ public class SelectTables {
   private JButton btnGenerate;
   private JButton btnSelectByRegex;
   private JPanel tablePanel;
+  private JRadioButton duplicateActionIgnoreButton;
+  private JRadioButton duplicateActionRewriteButton;
+  private JRadioButton duplicateActionMergeButton;
 }
