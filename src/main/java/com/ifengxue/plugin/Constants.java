14044272982f5189b7f1ec73ab3bef8324311f5f
Carter <liukefeng2008@hotmail.com>
2021-03-23 22:12:52 +0800

    Rename plugin name (Jpa Support) to JPA Support

--- src/main/java/com/ifengxue/plugin/Constants.java
+++ src/main/java/com/ifengxue/plugin/Constants.java
@@ -1,8 +1,9 @@
 package com.ifengxue.plugin;
 
 public interface Constants {
-  String PLUGIN_NAME = "Jpa Support";
-  String NAME = "JpaSupport";
+
+  String PLUGIN_NAME = "JPA Support";
+  String NAME = "JPASupport";
   String GROUP_ID = NAME;
   String JPA_ENTITY_TEMPLATE_ID = "template/JpaEntity.vm";
   String JPA_REPOSITORY_TEMPLATE_ID = "template/JpaRepository.vm";
