8c67b06d3e7a1e91b038e2c659f362527493bcae
Carter <liukefeng2008@hotmail.com>
2021-04-30 20:09:29 +0800

    feat(generator): 支持预览Bean和Repository

--- src/main/java/com/ifengxue/plugin/util/TestTemplateHelper.java
+++ src/main/java/com/ifengxue/plugin/util/TestTemplateHelper.java
@@ -21,7 +21,8 @@ import java.util.Collections;
 public enum TestTemplateHelper {
     ;
 
-    public static Object evaluate(Class<? extends AbstractSourceParser> clazz, String template) {
+    public static Object evaluate(Class<? extends AbstractSourceParser> clazz, Table table,
+        String template) {
         AutoGeneratorSettingsState settingsState = ServiceManager.getService(
             Holder.getOrDefaultProject(), AutoGeneratorSettingsState.class);
         GeneratorConfig config = new GeneratorConfig();
@@ -54,86 +55,95 @@ public enum TestTemplateHelper {
         AbstractSourceParser sourceParser = newInstance(clazz);
         sourceParser.setVelocityEngine(VelocityUtil.getInstance(), "UTF-8");
         try {
-            Table table = new Table();
-            table
-                .setTableComment("Example Table Comment")
-                .setTableName("t_example_table")
-                .setTableSchema("example_db")
-                .setEntityName(StringHelper.parseEntityName(settingsState.removeTablePrefix(table.getTableName())))
-                .setRepositoryName(table.getEntityName() + "Repository")
-                .setPackageName("org.example")
-                .setPrimaryKeyClassType(Long.class)
-                .setPrimaryKeyCount(1)
-                .setColumns(Arrays.asList(
-                    new Column()
-                        .setAutoIncrement(true)
-                        .setColumnName("f_id")
-                        .setSort(0)
-                        .setDbDataType("BIGINT")
-                        .setPrimary(true)
-                        .setAutoIncrement(true)
-                        .setColumnComment("Id"),
-                    new Column()
-                        .setColumnName("f_version")
-                        .setSort(4)
-                        .setDbDataType("BIGINT")
-                        .setColumnComment("Version number")
-                        .setDefaultValue("0"),
-                    new Column()
-                        .setColumnName("film_title")
-                        .setSort(8)
-                        .setDbDataType("VARCHAR")
-                        .setNullable(true)
-                        .setColumnComment("Film title")
-                        .setDefaultValue("Interstellar IMAX"),
-                    new Column()
-                        .setColumnName("film_description")
-                        .setSort(12)
-                        .setDbDataType("TEXT")
-                        .setNullable(true)
-                        .setColumnComment("Film description"),
-                    new Column()
-                        .setColumnName("film_language_id")
-                        .setSort(16)
-                        .setDbDataType("TINYINT")
-                        .setColumnComment("Film language id"),
-                    new Column()
-                        .setColumnName("film_rental_duration")
-                        .setSort(20)
-                        .setDbDataType("TINYINT")
-                        .setNullable(true)
-                        .setColumnComment("Film rental duration"),
-                    new Column()
-                        .setColumnName("film_rental_rate")
-                        .setSort(24)
-                        .setDbDataType("DECIMAL")
-                        .setColumnComment("Film rental rate")
-                        .setDefaultValue("4.99"),
-                    new Column()
-                        .setColumnName("f_created_at")
-                        .setSort(98)
-                        .setDbDataType("DATETIME")
-                        .setColumnComment("Created at")
-                        .setDefaultValue("CURRENT_TIMESTAMP"),
-                    new Column()
-                        .setColumnName("f_updated_at")
-                        .setSort(99)
-                        .setDbDataType("DATETIME")
-                        .setColumnComment("Updated at")
-                        .setDefaultValue("CURRENT_TIMESTAMP")
-                ))
-                .setSelected(true);
-            table.getColumns().forEach(column -> ColumnUtil
-                .parseColumn(column, settingsState.getRemoveFieldPrefix(),
-                    true, settingsState.isUseJava8DateType()));
             return SourceFormatter.formatJavaCode(sourceParser.parse(config, table, template));
         } catch (Exception ex) {
             return ex;
         }
     }
 
-    public static String evaluateToString(Class<? extends AbstractSourceParser> clazz, String template) {
-        Object evaluate = evaluate(clazz, template);
+    public static String evaluateToString(Class<? extends AbstractSourceParser> clazz,
+        String template) {
+        AutoGeneratorSettingsState settingsState = ServiceManager.getService(
+            Holder.getOrDefaultProject(), AutoGeneratorSettingsState.class);
+        Table table = new Table();
+        table
+            .setTableComment("Example Table Comment")
+            .setTableName("t_example_table")
+            .setTableSchema("example_db")
+            .setEntityName(
+                StringHelper.parseEntityName(settingsState.removeTablePrefix(table.getTableName())))
+            .setRepositoryName(table.getEntityName() + "Repository")
+            .setPackageName("org.example")
+            .setPrimaryKeyClassType(Long.class)
+            .setPrimaryKeyCount(1)
+            .setColumns(Arrays.asList(
+                new Column()
+                    .setAutoIncrement(true)
+                    .setColumnName("f_id")
+                    .setSort(0)
+                    .setDbDataType("BIGINT")
+                    .setPrimary(true)
+                    .setAutoIncrement(true)
+                    .setColumnComment("Id"),
+                new Column()
+                    .setColumnName("f_version")
+                    .setSort(4)
+                    .setDbDataType("BIGINT")
+                    .setColumnComment("Version number")
+                    .setDefaultValue("0"),
+                new Column()
+                    .setColumnName("film_title")
+                    .setSort(8)
+                    .setDbDataType("VARCHAR")
+                    .setNullable(true)
+                    .setColumnComment("Film title")
+                    .setDefaultValue("Interstellar IMAX"),
+                new Column()
+                    .setColumnName("film_description")
+                    .setSort(12)
+                    .setDbDataType("TEXT")
+                    .setNullable(true)
+                    .setColumnComment("Film description"),
+                new Column()
+                    .setColumnName("film_language_id")
+                    .setSort(16)
+                    .setDbDataType("TINYINT")
+                    .setColumnComment("Film language id"),
+                new Column()
+                    .setColumnName("film_rental_duration")
+                    .setSort(20)
+                    .setDbDataType("TINYINT")
+                    .setNullable(true)
+                    .setColumnComment("Film rental duration"),
+                new Column()
+                    .setColumnName("film_rental_rate")
+                    .setSort(24)
+                    .setDbDataType("DECIMAL")
+                    .setColumnComment("Film rental rate")
+                    .setDefaultValue("4.99"),
+                new Column()
+                    .setColumnName("f_created_at")
+                    .setSort(98)
+                    .setDbDataType("DATETIME")
+                    .setColumnComment("Created at")
+                    .setDefaultValue("CURRENT_TIMESTAMP"),
+                new Column()
+                    .setColumnName("f_updated_at")
+                    .setSort(99)
+                    .setDbDataType("DATETIME")
+                    .setColumnComment("Updated at")
+                    .setDefaultValue("CURRENT_TIMESTAMP")
+            ))
+            .setSelected(true);
+        table.getColumns().forEach(column -> ColumnUtil
+            .parseColumn(column, settingsState.getRemoveFieldPrefix(),
+                true, settingsState.isUseJava8DateType()));
+        return evaluateToString(clazz, table, template);
+    }
+
+    public static String evaluateToString(Class<? extends AbstractSourceParser> clazz, Table table,
+        String template) {
+        Object evaluate = evaluate(clazz, table, template);
         if (evaluate instanceof String) {
             return (String) evaluate;
         }
